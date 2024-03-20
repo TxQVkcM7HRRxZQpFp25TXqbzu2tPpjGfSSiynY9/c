264b9925bfe62c77bdbd1e859531f5208b407356
TxQVkcM7HRRxZQpFp25TXqbzu2tPpjGfSSiynY9 <147373367+TxQVkcM7HRRxZQpFp25TXqbzu2tPpjGfSSiynY9@users.noreply.github.com>
2024-03-20 12:04:47 +0100

    Update Zad1.c

--- Zadania4/Zad1.c
+++ Zadania4/Zad1.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+int factorial(int n) {
+    if (n <= 1) {
+        return 1;
+    }
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    printf("Podaj liczbę całkowitą: ");
+    scanf("%d", &num);
+    printf("Silnia z %d = %d\n", num, factorial(num));
+    return 0;
+}
