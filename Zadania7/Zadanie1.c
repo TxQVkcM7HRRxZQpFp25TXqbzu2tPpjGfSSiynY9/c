2cf0cbc7bcbc07e0e16f11d69ad0604cfb01e3e5
TxQVkcM7HRRxZQpFp25TXqbzu2tPpjGfSSiynY9 <147373367+TxQVkcM7HRRxZQpFp25TXqbzu2tPpjGfSSiynY9@users.noreply.github.com>
2024-05-10 20:40:41 +0200

    Update Zadanie1.c

--- Zadania7/Zadanie1.c
+++ Zadania7/Zadanie1.c
@@ -1,59 +1,74 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
-#include <ctype.h>
+#include <stdlib.h>
 
-// Funkcja do konwertowania słów na liczby
-int wordToNumber(char* word) {
+int word_to_digit(const char* word) {
     if (strcmp(word, "one") == 0) return 1;
-    if (strcmp(word, "two") == 0) return 2;
-    if (strcmp(word, "three") == 0) return 3;
-    if (strcmp(word, "four") == 0) return 4;
-    if (strcmp(word, "five") == 0) return 5;
-    if (strcmp(word, "six") == 0) return 6;
-    if (strcmp(word, "seven") == 0) return 7;
-    if (strcmp(word, "eight") == 0) return 8;
-    if (strcmp(word, "nine") == 0) return 9;
-    return 0;
+    else if (strcmp(word, "two") == 0) return 2;
+    else if (strcmp(word, "three") == 0) return 3;
+    else if (strcmp(word, "four") == 0) return 4;
+    else if (strcmp(word, "five") == 0) return 5;
+    else if (strcmp(word, "six") == 0) return 6;
+    else if (strcmp(word, "seven") == 0) return 7;
+    else if (strcmp(word, "eight") == 0) return 8;
+    else if (strcmp(word, "nine") == 0) return 9;
+    return -1;
 }
 
-// Funkcja do sumowania wartości energii
-int sumOfEnergies(char* filename) {
-    FILE* file = fopen(filename, "r");
-    if (!file) {
-        printf("Cannot open the file %s\n", filename);
-        return -1;
-    }
+void convert_words_to_digits(char* line) {
+    char* result = malloc(strlen(line) + 1);
+    char* token, * original = line, * res_ptr = result;
+    int digit;
 
-    char word[100];
-    int sum = 0;
-
-    while (fscanf(file, "%s", word) != EOF) {
-        int length = strlen(word);
-        // Sprawdź czy słowo jest liczbą
-        if (isdigit(word[0])) {
-            int first = word[0] - '0';
-            int last = word[length - 1] - '0';
-            sum += (first + last) * 10;
+    token = strtok(line, " ,.-");
+    while (token != NULL) {
+        digit = word_to_digit(token);
+        if (digit != -1) {
+            *res_ptr++ = '0' + digit;
         }
         else {
-            // Sprawdź czy słowo jest liczbą zapisaną słownie
-            int number = wordToNumber(word);
-            if (number > 0) {
-                sum += number;
+            while (*token) {
+                *res_ptr++ = *token++;
             }
+        }
+        token = strtok(NULL, " ,.-");
+    }
+    *res_ptr = '\0';
+    strcpy(original, result);
+    free(result);
+}
+
+int main() {
+    FILE* file = fopen("input.txt", "r");
+    char line[256];
+    int total = 0;
+
+    if (file == NULL) {
+        printf("Nie udało się otworzyć pliku.\n");
+        return 1;
+    }
+
+    while (fgets(line, sizeof(line), file)) {
+        char* ptr = line;
+        int first_digit = -1, last_digit = -1;
+
+        convert_words_to_digits(line);
+
+        while (*ptr) {
+            if (*ptr >= '0' && *ptr <= '9') {
+                if (first_digit == -1) first_digit = *ptr - '0';
+                last_digit = *ptr - '0';
+            }
+            ptr++;
+        }
+
+        if (first_digit != -1 && last_digit != -1) {
+            total += first_digit * 10 + last_digit;
         }
     }
 
     fclose(file);
-    return sum;
-}
+    printf("Całkowita wartość energii: %d\n", total);
 
-int main() {
-    char filename[] = "input.txt";
-    int result = sumOfEnergies(filename);
-    if (result != -1) {
-        printf("Sum of energies: %d\n", result);
-    }
     return 0;
 }
